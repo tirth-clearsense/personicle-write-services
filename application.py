4656065d19660373e2b737407e83867b27bb2fe2
tirth-clearsense <tpatel@clearsense.com>
2022-05-26 08:53:25 -0400

    test

--- application.py
+++ application.py
@@ -301,5 +301,5 @@ def delete_user_account(u_id,delete_user_headers,events_found=None):
 if __name__ == "__main__":
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     print("running server on {}:{}".format(HOST_CONFIG['HOST_URL'], HOST_CONFIG['HOST_PORT']))
-    # app.run(HOST_CONFIG['HOST_URL'], port=HOST_CONFIG['HOST_PORT'], debug=True)#, ssl_context='adhoc')
-    app.run(debug=True)
+    app.run(HOST_CONFIG['HOST_URL'], port=HOST_CONFIG['HOST_PORT'], debug=True)#, ssl_context='adhoc')
+    # app.run(debug=True)
